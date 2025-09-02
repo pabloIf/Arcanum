97c9ce2e8944cda8f2ad0087f2be1889a1885cd6
Pavlo Saltikov <aknelsaltikov@email.com>
2025-09-03 00:07:10 +0300

    did core models

--- arcanum/core/models.py
+++ arcanum/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=164, db_index=True)
+    slug = models.SlugField(max_length=164, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=164, db_index=True)
+    slug = models.SlugField(max_length=164, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
