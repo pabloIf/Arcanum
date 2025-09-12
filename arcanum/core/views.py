3761325a74a2e4af352f5814f0eab5cf7a8f42ad
Pavlo Saltikov <aknelsaltikov@email.com>
2025-09-12 15:42:43 +0300

    delete comments

--- arcanum/core/views.py
+++ arcanum/core/views.py
@@ -35,28 +35,6 @@ def product_list(request, category_slug=None):
                    'query': query,
     })
 
-# def product_list(request, category_slug=None):
-#     categories = Category.objects.all()
-#     products = Product.objects.filter(available=True).exclude(image='')
-
-#     category = None
-#     if category_slug:
-#         category = get_object_or_404(Category, slug=category_slug)
-#         products = products.filter(category=category)
-
-#     featured_product = None
-#     if not category:
-#         featured_product = Product.objects.filter(is_featured=True).first()
-#         if featured_product:
-#             products = products.exclude(id=featured_product.id)
-
-#     return render(request, 'core/product/list.html',
-#                   {'products' : products,
-#                    'categories' : categories,
-#                    'category' : category,
-#                    'featured_product': featured_product,
-#     })
-
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     related_products = Product.objects.filter(category=product.category, available=True).exclude(id=product.id)[:4]
