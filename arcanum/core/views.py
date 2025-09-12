fd58530315529b7c43e0f561ea592b6e4e9bb73c
Pavlo Saltikov <aknelsaltikov@email.com>
2025-09-12 15:20:10 +0300

    mede a search by slug

--- arcanum/core/views.py
+++ arcanum/core/views.py
@@ -1,27 +1,61 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.utils.text import slugify
+
 from .models import Category, Product
+
 
 def product_list(request, category_slug=None):
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True).exclude(image='')
+    query = request.GET.get('q', '')
 
-    category = None
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+    if query:
+        query_slug = slugify(query)
+        products = Product.objects.filter(available=True, slug__icontains=query_slug).exclude(image='')
+        category = None
+        featured_product = None
+    else:
+        products = Product.objects.filter(available=True).exclude(image='')
+        category = None
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
 
-    featured_product = None
-    if not category:
-        featured_product = Product.objects.filter(is_featured=True).first()
-        if featured_product:
-            products = products.exclude(id=featured_product.id)
+        featured_product = None
+        if not category:
+            featured_product = Product.objects.filter(is_featured=True).first()
+            if featured_product:
+                products = products.exclude(id=featured_product.id)
 
     return render(request, 'core/product/list.html',
                   {'products' : products,
                    'categories' : categories,
                    'category' : category,
                    'featured_product': featured_product,
+                   'query': query,
     })
+
+# def product_list(request, category_slug=None):
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True).exclude(image='')
+
+#     category = None
+#     if category_slug:
+#         category = get_object_or_404(Category, slug=category_slug)
+#         products = products.filter(category=category)
+
+#     featured_product = None
+#     if not category:
+#         featured_product = Product.objects.filter(is_featured=True).first()
+#         if featured_product:
+#             products = products.exclude(id=featured_product.id)
+
+#     return render(request, 'core/product/list.html',
+#                   {'products' : products,
+#                    'categories' : categories,
+#                    'category' : category,
+#                    'featured_product': featured_product,
+#     })
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
@@ -31,5 +65,4 @@ def product_detail(request, id, slug):
                   {"product" : product,
                   'related_products' : related_products,
     })
-
 
